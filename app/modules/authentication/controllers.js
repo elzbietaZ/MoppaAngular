d7602675cb2206a3b2d34a9ad1f2b1d33a979883
elzbietaZ <elzbieta.zukrowska@gmail.com>
2016-01-12 22:31:09 +0100

    back to the old version cause karma was working but app stopped

--- app/modules/authentication/controllers.js
+++ app/modules/authentication/controllers.js
@@ -1,6 +1,6 @@
 'use strict';
  
-angular.module('Authentication',[])
+angular.module('Authentication')
  
 .controller('LoginController',
     ['$scope', '$rootScope', '$location', 'AuthenticationService',
